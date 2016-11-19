fb8674bd9bb15a25a1f0bb00eb997d7fdc75428f
Berend Wouters <berend.wouters@github.com>
2016-11-19 14:09:42 +0100

    Add project files.

---
+++ ESP8266Server/ESP8266Server.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP8266Server.ino
+ Created:	11/19/2016 2:09:38 PM
+ Author:	Berend
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
