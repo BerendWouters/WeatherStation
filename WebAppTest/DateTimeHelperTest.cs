8d2156598f1c031831deee75f7f26f42ef681dd8
Berend Wouters <berend.wouters@gmail.com>
2017-03-19 18:43:41 +0100

    Adds unit test for unixtimehelper

---
+++ WebAppTest/DateTimeHelperTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Helpers;
+
+namespace WebAppTest
+{
+    [TestClass]
+    public class DateTimeHelperTest
+    {
+        [TestMethod]
+        public void GetUnixTimeStamp_Valid()
+        {
+            var unixTimeStamp = DateTimeHelpers.GetUnixTimeStamp(DateTime.Parse("2017-03-19 18:34:15"));
+            Assert.AreEqual(1489948455, unixTimeStamp);
+        }
+    }
+}
